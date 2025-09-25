c493adca44435b25945dcd3d54d09e23821fb40c
Yae Miko <aloksinghkumar756@gmail.com>
2025-09-25 19:42:24 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -197,7 +197,7 @@ async def post_main_channel_summary(name, aniInfo, channel_details):
         caption += f"<b>➤ Episode - {episode_info['episode']}</b>\n"
         caption += f"<b>➤ Quality: {episode_info['quality']}</b>\n"
         caption += f"<b>────────────────────────────</b>\n"
-        caption += f"<blockquote><b>‣ Synopsis : {synopsis}</b></blockquote>"
+        caption += f"<blockquote expandable><b>‣ Synopsis : {synopsis}</b></blockquote>"
         
         # Create join button
         keyboard = None
